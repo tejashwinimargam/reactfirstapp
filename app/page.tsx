7eb675a46bbc262a257dc1493c8a05bba841c68e
tejashwini <teju5margam@gmail.com>
2025-04-06 16:00:18 +0530

    commit2app2

---
+++ app/page.tsx
@@ -0,0 +1,12 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div >
+      <h1>welcome to my app2</h1>
+      <button className="custom-button">I'm a button</button>
+           
+    </div>
+  );
+}
+
